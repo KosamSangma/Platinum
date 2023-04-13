571cfc1e3a19c4d7562c779e73af9e49a6349132
Kosam <waldokosam@gmail.com>
2023-04-13 17:31:54 +0530

    Navbar and react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,16 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 function App() {
-  return <div>My App</div>
+  return (
+    <ChakraProvider>
+      <Router>
+        <Navbar>
+          <main>{/* <Routes></Routes> */}</main>
+        </Navbar>
+      </Router>
+    </ChakraProvider>
+  )
 }
 
 export default App
